dfe3c18153de1663bf084a9c62cd2673c59d3ce4
Alireza6677 <alireza6677@gmail.com>
2021-03-28 13:18:26 +0430

    Support GNOME 40

--- extension.js
+++ extension.js
@@ -29,9 +29,9 @@ const InvertWindowEffect = new Lang.Class({
 		';
 	},
 
-	vfunc_paint_target: function(paint_context) {
+	vfunc_paint_target: function(node, paint_context) {
 		this.set_uniform_value("tex", 0);
-		this.parent(paint_context);
+		this.parent(node, paint_context);
 	}
 });
 
